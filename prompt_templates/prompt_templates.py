f7e258698ebd3231941869defc3f3e8ff95ff6be
anix001 <chaudharyanish305@gmail.com>
2025-02-18 15:15:56 +0545

    Feat: Prompt Templates in langchain

---
+++ prompt_templates/prompt_templates.py
@@ -0,0 +1,19 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+
+template = """ Write a{tone} email to {company} expressing interest in the {position} position, mentioning {skill} as a 
+key strength. keep it to max 4 lines"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = OllamaLLM(model="llama3.2:latest")
+
+chain = prompt | model
+
+msg = chain.invoke({
+    "tone": "energetic",
+    "company": "Samsung",
+    "position": "AI Engineer",
+    "skill":"AI"
+    })
+print(msg)
